54f9473b7dbc12771bdfe01a10153611c9afe9a9
Parsa307 <79400634+Parsa307@users.noreply.github.com>
2024-12-31 16:06:54 +0330

    Renamed buyButton to saleButton

--- Sale.js
+++ Sale.js
@@ -90,7 +90,7 @@
   // We store our auto-stop timeout ID so we can clear it if we stop early
   const autoStopId = setTimeout(() => stopAll("Script ended after time limit."), STOP_AFTER);
 
-  // Repeatedly set values & click the buy button
+  // Repeatedly set values & click the Sale button
   const intervalId = setInterval(() => {
     // 1) Check valueInput range
     if (!isWithinRange(valueInput, valueStr)) {
@@ -112,13 +112,13 @@
     emulateTyping(priceInput, priceValue);
     console.log(`Typed "${priceValue}" into price input (:r5:).`);
 
-    // 3) Click the Buy button
-    const buyButton = document.querySelector("button.online-tools-sncy46");
-    if (buyButton) {
-      buyButton.click();
-      console.log("Buy button clicked.");
+    // 3) Click the Sale button
+    const saleButton = document.querySelector("button.online-tools-sncy46");
+    if (saleButton) {
+      saleButton.click();
+      console.log("Sale button clicked.");
     } else {
-      console.log("Buy button not found.");
+      console.log("Sale button not found.");
     }
   }, INTERVAL_MS);
 
